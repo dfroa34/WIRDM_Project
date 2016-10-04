3d40f244345cea07bfe3263cf5b62cf18f56ff05
二木君 <leolin0720@gmail.com>
2016-10-04 17:07:41 +0200

    Store raw tweets

--- tweetretrieval.py
+++ tweetretrieval.py
@@ -14,6 +14,12 @@ ckey = 'IhyKiqN6JJgvmeBQeBKiPoCtX'
 csecret = 'DPiWDUCymkYxNYBMkwaTkZD85oHDwC23gtOJ2woE0BFaCrJo3Z'
 atoken = '2609369460-otRuCCeuDmZxOwug2gJZlhF3PKPNA4tn0msGwH9'
 asecret = 'mejOpj5q4mgg03wMOxBTVuZ6ZOjiTuxhBpMGFne1Cj4c0'
+"""
+            A	|B	 |C	|D	  |E	  |F		    |G		 |H		|I	    |J	 |K
+            keyword	|UserName|UserID|TweetText|TweetID|DateOfPublication|#ofFollowers|#ofFollowings	|#ofStatuses|URLs|Label
+            No keyword and label
+        """
+
 
 class listener(StreamListener):
     def on_data(self, data):
@@ -22,26 +28,20 @@ class listener(StreamListener):
             with open("backup.txt", 'a') as backup:
                 backup.write(str(all_data) + "\n")
                 backup.close()
-        """
 
-A	|B	 |C	|D	  |E	  |F		    |G		 |H		|I	    |J	 |K
-keyword	|UserName|UserID|TweetText|TweetID|DateOfPublication|#ofFollowers|#ofFollowings	|#ofStatuses|URLs|Label
-
-        No keyword and label
-        """
+            """
             text = str(all_data["text"]).encode("utf-8")
             id = str(all_data["id"]).encode("utf-8")
             timestamp = str(all_data["timestamp_ms"]).encode("utf-8")
             sn = str(all_data["user"]["screen_name"]).encode("utf-8")
+
             user_id = str(all_data["user"]["id"]).encode("utf-8")
             create = str(all_data["created_at"]).encode("utf-8")
-            follower = str(all_data["user"]["follower_count"]).encode("utf-8")
+            follower = str(all_data["user"]["followers_count"]).encode("utf-8")
             following = str(all_data["user"]["following"]).encode("utf-8")
             status = str(all_data["user"]["statuses_count"]).encode("utf-8")
-            url = str(all_data["urls"]["url"]).encode("utf-8")
 
-        # text = data.split(',"text":"')[1].split('","source')[0]
-        # name = data.split(',"screen_name":"')[1].split('","location')[0]
+
             contentlist = []
             contentlist.append(text)
             contentlist.append(id)
@@ -52,18 +52,18 @@ keyword	|UserName|UserID|TweetText|TweetID|DateOfPublication|#ofFollowers|#ofFol
             contentlist.append(follower)
             contentlist.append(following)
             contentlist.append(status)
-            contentlist.append(url)
+
             print contentlist
             f = open("tweets3.csv", 'ab')
             wrt = csv.writer(f, dialect='excel')
             try:
                 wrt.writerow(contentlist)
-            except UnicodeEncodeError, UnicodeEncodeError:
+            except UnicodeEncodeError:
                 return True
+            """
             return True
         except BaseException, e:
-
-            print 'failed on data',type(e),str(e)
+            print 'failed on data', type(e), str(e)
             time.sleep(3)
 
     def on_error(self, status):
@@ -73,5 +73,4 @@ keyword	|UserName|UserID|TweetText|TweetID|DateOfPublication|#ofFollowers|#ofFol
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
-Stream.userstream()
 twitterStream.filter(track=["zika"], languages=['en'])
