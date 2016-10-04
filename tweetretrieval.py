3aabc8b9fee9862345952627a565ea8084677e8a
二木君 <leolin0720@gmail.com>
2016-10-04 12:55:09 +0200

    Changed the code to make a backup for all data

--- tweetretrieval.py
+++ tweetretrieval.py
@@ -3,46 +3,63 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import time
 import json
-
+import csv
 
 """
 import csv
 import sys
 """
-ckey = 'dspa2PuUl7kNZjjpxmeJeh4WO'
-csecret = 'fKJ4zB6pS6bEVZShNKjfUIC6Qk2YjtL20U8qr3goQQuIAPJsEI'
-atoken = '27402509-6zlqaqSnr44DgimjhChNHlawxcEiRjJSCrjwdLbFt'
-asecret = '9wzHsutuJFota3HFFaFPX8WXT8S0fp13d76BNboV55xu5'
+
+ckey = 'IhyKiqN6JJgvmeBQeBKiPoCtX'
+csecret = 'DPiWDUCymkYxNYBMkwaTkZD85oHDwC23gtOJ2woE0BFaCrJo3Z'
+atoken = '2609369460-otRuCCeuDmZxOwug2gJZlhF3PKPNA4tn0msGwH9'
+asecret = 'mejOpj5q4mgg03wMOxBTVuZ6ZOjiTuxhBpMGFne1Cj4c0'
 
 class listener(StreamListener):
-    
-        def on_data(self,data):
-            try:    
-                #print data
-                all_data = json.loads(data)
+    def on_data(self, data):
+        try:
+            all_data = json.loads(data)
+            with open("backup.txt", 'a') as backup:
+                backup.write(str(all_data) + "\n")
+                backup.close()
 
-                text = all_data["text"]
-                                
-                #text = data.split(',"text":"')[1].split('","source')[0]                
-                #name = data.split(',"screen_name":"')[1].split('","location')[0]                        
-                
-                print text 
-                                
-                saveThis = str(time.time())+'::'+text
-                saveFile = open('tweets3.csv','a')
-                saveFile.write(saveThis)
-                saveFile.write('\n')
-                saveFile.close()
-            except BaseException, e:
-                print 'failed on data',str(e)
-                time.sleep(5)
+            text = str(all_data["text"]).encode("utf-8")
 
+        # print "text" + text
+            id = str(all_data["id"]).encode("utf-8")
+
+        # print "id" + str(id)
+            timestamp = str(all_data["timestamp_ms"]).encode("utf-8")
+
+        # print "stimestamp" + str(timestamp)
+            sn = str(all_data["user"]["screen_name"]).encode("utf-8")
+
+        # text = data.split(',"text":"')[1].split('","source')[0]
+        # name = data.split(',"screen_name":"')[1].split('","location')[0]
+            contentlist = []
+            contentlist.append(text)
+            contentlist.append(id)
+            contentlist.append(timestamp)
+            contentlist.append(sn)
+            print contentlist
+            f = open("tweets3.csv", 'ab')
+            wrt = csv.writer(f, dialect='excel')
+            try:
+                wrt.writerow(contentlist)
+            except UnicodeEncodeError, UnicodeEncodeError:
+                return True
             return True
-        
-        def on_error (self,status):
-            print status
-                 
-auth = OAuthHandler (ckey, csecret)
+        except BaseException, e:
+
+            print 'failed on data',type(e),str(e)
+            time.sleep(3)
+
+    def on_error(self, status):
+        print "Error status:" + str(status)
+
+
+auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
+Stream.userstream()
 twitterStream.filter(track=["zika"], languages=['en'])
